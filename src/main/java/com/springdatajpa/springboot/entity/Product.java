af820c9ddef5b01b748215ab6bf05badf6defaa6
Fernando Salas <luisfernandosalasg@gmail.com>
2023-06-04 00:10:24 -0500

    Add @createTimeStamp and @updateTimeStamp

--- src/main/java/com/springdatajpa/springboot/entity/Product.java
+++ src/main/java/com/springdatajpa/springboot/entity/Product.java
@@ -1,6 +1,8 @@
 package com.springdatajpa.springboot.entity;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -34,7 +36,9 @@ public class Product {
     private BigDecimal price;
     private boolean active;
     private String imageUrl;
+    @CreationTimestamp
     private LocalDateTime dateCreated;
+    @UpdateTimestamp
     private LocalDateTime lastUpdated;
 
     public Long getId() {
