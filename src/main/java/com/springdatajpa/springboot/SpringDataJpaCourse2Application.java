277b8b5e94bd1bde6467166353352ae3262502ca
Fernando Salas <luisfernandosalasg@gmail.com>
2023-06-03 22:06:53 -0500

    Basic config added

---
+++ src/main/java/com/springdatajpa/springboot/SpringDataJpaCourse2Application.java
@@ -0,0 +1,13 @@
+package com.springdatajpa.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaCourse2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaCourse2Application.class, args);
+	}
+
+}
