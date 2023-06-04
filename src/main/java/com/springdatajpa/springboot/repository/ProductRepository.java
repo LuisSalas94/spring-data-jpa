063f71c51320a0025b69ecf9623fc9a5338cb4e3
Fernando Salas <luisfernandosalasg@gmail.com>
2023-06-04 18:56:27 -0500

    Custom Query Method findBy()

--- src/main/java/com/springdatajpa/springboot/repository/ProductRepository.java
+++ src/main/java/com/springdatajpa/springboot/repository/ProductRepository.java
@@ -3,5 +3,19 @@ package com.springdatajpa.springboot.repository;
 import com.springdatajpa.springboot.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    /*
+    * Returns the found product entry by using its name as search
+    * criteria. If not product entry is found, this method returns null;
+    * */
+    public Product findByName(String name);
+    public Product findByDescription(String description);
+
+    /*
+    * Returns an Optional which contains the found product entry by using its od as search
+    * criteria. If no product entry is found, this method returns an empty Optional
+    * */
+    Optional<Product> findById(Long id);
 }
---
+++ src/test/java/com/springdatajpa/springboot/repository/QueryMethodTest.java
@@ -0,0 +1,36 @@
+package com.springdatajpa.springboot.repository;
+
+import com.springdatajpa.springboot.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@SpringBootTest
+public class QueryMethodTest {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void findByNameMethod() {
+        Product product = productRepository.findByName("product 2");
+        System.out.println("Id: " + product.getId());
+        System.out.println("Name: " + product.getName());
+        System.out.println("Description: " + product.getDescription());
+    }
+
+    @Test
+    void findByDescriptionMethod() {
+        Product product = productRepository.findByDescription("product 2 description");
+        System.out.println("Description: " + product.getDescription());
+    }
+
+    @Test
+    void findByIdMethod() {
+        Product product = productRepository.findById(1L).get();
+        System.out.println("Id: " + product.getId());
+        System.out.println("Name: " + product.getName());
+        System.out.println("Description: " + product.getDescription());
+    }
+}
