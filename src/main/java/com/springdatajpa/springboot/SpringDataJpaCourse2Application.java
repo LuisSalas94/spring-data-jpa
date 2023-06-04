c6dc4ecf92fd80e6d265a903546399a9e0a4a295
Fernando Salas <luisfernandosalasg@gmail.com>
2023-06-03 22:29:56 -0500

    Create MYSQL connection

--- src/main/java/com/springdatajpa/springboot/SpringDataJpaCourse2Application.java
+++ src/main/java/com/springdatajpa/springboot/SpringDataJpaCourse2Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringDataJpaCourse2Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataJpaCourse2Application.class, args);
 	}
